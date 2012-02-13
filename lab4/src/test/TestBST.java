aece9e5dcb051605aafa03fc4abd5c2c5caeb886
Anton Eliasson <devel@antoneliasson.se>
2012-02-13 21:31:57 +0100

    Klar med förberedelserna inför labb 4

---
+++ lab4/src/test/TestBST.java
@@ -0,0 +1,40 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import bst.BinarySearchTree;
+
+public class TestBST {
+	private BinarySearchTree<Integer> bst;
+	
+	@Before
+	public void setUp() throws Exception {
+		bst = new BinarySearchTree<Integer>();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		bst = null;
+	}
+	
+	@Test
+	public void testHeight() {
+		assertEquals("Fel höjd", bst.height(), 0);
+		
+		bst.add(4);
+		bst.add(2);
+		bst.add(1);
+		bst.add(3);
+		bst.add(6);
+		bst.add(7);
+		bst.add(5);
+		
+		assertEquals("Fel höjd", bst.height(), 3);
+		
+		bst.printTree();
+	}
+
+}
